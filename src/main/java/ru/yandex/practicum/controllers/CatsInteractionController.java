a3ee0fd3a81891c5c135bf7ebd14df5d36e23350
Zakharov Ivan <>
2026-04-07 17:06:05 +0300

    1

--- src/main/java/ru/yandex/practicum/controllers/CatsInteractionController.java
+++ src/main/java/ru/yandex/practicum/controllers/CatsInteractionController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controllers;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/cats")
 public class CatsInteractionController {
+
     private int happiness = 0;
 
     @GetMapping("/converse")
@@ -20,6 +22,13 @@ public class CatsInteractionController {
 
     @GetMapping("/pet")
     public Map<String, String> pet(@RequestParam(required = false) final Integer count) {
+        if (count == null) {
+            throw new IncorrectCountException("Параметр count равен null.");
+        }
+        if (count <= 0) {
+            throw new IncorrectCountException("Параметр count имеет отрицательное значение.");
+        }
+
         happiness += count;
         return Map.of("talk", "Муррр. ".repeat(count));
     }
@@ -27,5 +36,15 @@ public class CatsInteractionController {
     @GetMapping("/happiness")
     public Map<String, Integer> happiness() {
         return Map.of("happiness", happiness);
+    }
+
+    @ExceptionHandler
+    public Map<String, String> handle(final IncorrectCountException  e) {
+        return Map.of("error", "Ошибка с параметром count.", "errorMessage", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public Map<String, String> handleRuntimeException(final RuntimeException e) {
+        return Map.of("error", "Произошла ошибка!");
     }
 }
